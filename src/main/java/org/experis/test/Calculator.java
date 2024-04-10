fe713d35248214abd1a2ec83d0e3d04d5b4bca3d
albertostizzoli <albertostizzoli@yahoo.it>
2024-04-10 16:21:36 +0200

    create class Calculator Test

--- src/main/java/org/experis/test/Calculator.java
+++ src/main/java/org/experis/test/Calculator.java
@@ -11,23 +11,6 @@ public class Calculator {
         this.num2 = num2;
     }
 
-    //Getter e Setter
-    public float getNum1(){
-        return num1;
-    }
-
-    public void setNum1(float num1){
-        this.num1 = num1;
-    }
-
-    public float getNum2(){
-        return num2;
-    }
-
-    public void setNum2(float num2){
-        this.num2 = num2;
-    }
-
     // Metodi
     public float add(float num1, float num2){
         return num1 + num2;
@@ -37,7 +20,10 @@ public class Calculator {
         return num1 - num2;
     }
 
-    public float divide(float num1, float num2){
+    public float divide(float num1, float num2) throws IllegalArgumentException{
+        if (num2 == 0){
+            throw new IllegalArgumentException("Il numero non si può dividere per 0");
+        }
          return num1 / num2;
     }
 
--- src/test/java/org/experis/test/CalculatorTest.java
+++ src/test/java/org/experis/test/CalculatorTest.java
@@ -1,4 +1,55 @@
 package org.experis.test;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setUp(){
+        // inizializzo un nuovo oggetto Calculator con valori predefiniti
+        calculator = new Calculator(0.0f, 0.0f);
+    }
+
+    @Test
+    // Questo test ha un nome descrittivo che verrà visualizzato nell'output dei test
+    @DisplayName("Test Addizione")
+    public void testAdd(){
+        // eseguo l'operazione di addizione
+        float result = calculator.add(3.3f, 2.3f);
+        // verifico  il risultato dell'operazione
+        assertEquals(5.6f, result);
+    }
+
+    @Test
+    @DisplayName("Test Sottrazione")
+    public void testSubtract(){
+        float result = calculator.subtract(3.0f, 2.0f);
+        assertEquals(1.0f, result);
+    }
+
+    @Test
+    @DisplayName("Test Moltiplicazione")
+    public void testMultiply(){
+        float result = calculator.multiply(3.0f, 3.0f);
+        assertEquals(9.0f, result);
+    }
+
+    @Test
+    @DisplayName("Test Divisione")
+    public void testDivide(){
+        float result = calculator.divide(6.0f, 3.0f);
+        assertEquals(2.0f, result);
+    }
+
+    @Test
+    @DisplayName("Test Divisione Per Zero")
+    public void testDivideByZero(){
+        // verifico se viene sollevata un'eccezione quando si cerca di dividere per zero
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(5.0f, 0.0f));
+    }
 }
+
