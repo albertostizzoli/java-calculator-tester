32e793fb641ce38c85090f29e1d501c05e86f47f
albertostizzoli <albertostizzoli@yahoo.it>
2024-04-10 16:34:53 +0200

    added other operations

--- src/test/java/org/experis/test/CalculatorTest.java
+++ src/test/java/org/experis/test/CalculatorTest.java
@@ -20,29 +20,37 @@ public class CalculatorTest {
     public void testAdd(){
         // eseguo l'operazione di addizione
         float result = calculator.add(3.3f, 2.3f);
+        float result2 = calculator.add(2.8f, 3.4f);
         // verifico  il risultato dell'operazione
         assertEquals(5.6f, result);
+        assertEquals(6.2f, result2);
     }
 
     @Test
     @DisplayName("Test Sottrazione")
     public void testSubtract(){
         float result = calculator.subtract(3.0f, 2.0f);
+        float result2 = calculator.subtract(7.5f, 3.4f);
         assertEquals(1.0f, result);
+        assertEquals(4.1f, result2);
     }
 
     @Test
     @DisplayName("Test Moltiplicazione")
     public void testMultiply(){
         float result = calculator.multiply(3.0f, 3.0f);
+        float result2 = calculator.multiply(8.0f, 4.0f);
         assertEquals(9.0f, result);
+        assertEquals(32.0, result2);
     }
 
     @Test
     @DisplayName("Test Divisione")
     public void testDivide(){
         float result = calculator.divide(6.0f, 3.0f);
+        float result2 = calculator.divide(10.0f, 2.0f);
         assertEquals(2.0f, result);
+        assertEquals(5.0f, result2);
     }
 
     @Test
@@ -50,6 +58,7 @@ public class CalculatorTest {
     public void testDivideByZero(){
         // verifico se viene sollevata un'eccezione quando si cerca di dividere per zero
         assertThrows(IllegalArgumentException.class, () -> calculator.divide(5.0f, 0.0f));
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(7.0f, 0.0f));
     }
 }
 
